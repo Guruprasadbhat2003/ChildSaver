2bc86a4cb0070c4b65ec5c737bb506d8cc8ab778
Guruprasad2003 <gurubhatkodani@gmail.com>
2025-05-14 00:43:26 +0530

    final block chain project added

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+export const donationAddress = "0x218C575B0062c3c3D199a6E76566efc3Fad301a9";
+export const contractAddress = "0x94Bf81513dB5A7458CB853cc54866eDa0619218B";
+export const contractABI = abi.abi;
